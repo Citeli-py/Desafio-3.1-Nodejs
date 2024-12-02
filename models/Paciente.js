45fa8d9c121c286c27d64be76066f4164e389a22
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-02 09:06:28 -0300

    :sparkles: feat: Classe Paciente alterada para o sequelize

--- models/Paciente.js
+++ models/Paciente.js
@@ -1,19 +1,39 @@
+import { Model, DataTypes } from "sequelize";
+
 /**
 * Classe que representa um Paciente.
 */
-export class Paciente {
+export class Paciente extends Model{
     /**
     * @property {string} cpf - CPF do paciente.
-    */
-    cpf;
-
-    /**
     * @property {string} nome - Nome do paciente.
-    */
-    nome;
+    * @property {DateTime} data_nasc - Data de nascimento do paciente no formato ISO (yyyy-MM-dd).
+    */    
 
-    /**
-    * @property {DateTime} data_nasc - Data de nascimento do paciente como uma instância de Luxon.
-    */
-    data_nasc;
+    static init(sequelize){
+        super.init({
+            cpf: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            data_nasc: {
+                type: DataTypes.DATEONLY,
+                /*Esse getter é utilizado para ser compativel com as outras partes do código que utilizam o luxon, 
+                sem a necessidade de refatorar tudo*/
+                get() {
+                    const rawValue = this.getDataValue("data_nasc")
+                    return rawValue ? DateTime.fromFormat(rawValue, "yyyy-MM-dd") : null;
+                },
+            },
+
+        }, {sequelize, modelName: "paciente", tableName: "pacientes", timestamps: false,})
+    }
 };
