521c05ed79621c1cad99ec4101048b87030925c8
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-06 21:41:21 -0300

    :sparkles: feat: Controller adaptado para lidar com o banco de dados e chamdas assincronas

--- controllers/ConsultaController.js
+++ controllers/ConsultaController.js
@@ -2,19 +2,15 @@ import { ConsultaBuilder } from "../models/ConsultaBuilder.js";
 import { DateTime } from "luxon";
 import { ErrorCodes } from "../utils/Error.js";
 import { Consulta } from "../models/Consulta.js";
-import { PacienteController } from "./PacienteController.js";
+import PacienteController from "./PacienteController.js";
+
+import { Op } from "sequelize";
 
 /**
-* Classe responsável por gerenciar consultas odontológicas.
+* Classe Singleton responsável por gerenciar consultas odontológicas.
 * Permite adicionar, remover e listar consultas, além de realizar validações e verificar agendamentos futuros.
 */
-export class ConsultaController{
-
-    /**
-    * Mapa que armazena consultas associadas aos CPFs dos pacientes.
-    * @type {Map<string, Consulta>}
-    */
-    consultas;
+class ConsultaController{
 
     /**
     * Builder para criar objetos de consulta.
@@ -24,11 +20,14 @@ export class ConsultaController{
 
     /**
     * Construtor da classe ConsultaController.
-    * Inicializa o mapa de consultas e o ConsultaBuilder.
+    * Inicializa o ConsultaBuilder.
     */
     constructor(){
-        this.consultas = new Map();
+        if (ConsultaController.instance) 
+            return ConsultaController.instance; // Retorna a instância existente
+        
         this.consulta_builder = new ConsultaBuilder();
+        ConsultaController.instance = this; // Salva a instância
     }
 
     /**
@@ -41,13 +40,18 @@ export class ConsultaController{
     /**
     * Define o CPF do paciente para a consulta em criação.
     * 
+    * @async
     * @param {string} cpf - CPF do paciente.
-    * @param {PacienteController} paciente_controller - Controlador de pacientes para validar o CPF.
     * @returns {{success: boolean, error?: number}} Objeto contendo o status da operação e, em caso de erro, um código de erro.
     */
-    setCpf(cpf, paciente_controller){
-        if(!paciente_controller.exists(cpf))
+    async setCpf(cpf){
+        // Berifica se o paciente existe
+        if(!(await PacienteController.exists(cpf)))
             return {success: false, error: ErrorCodes.ERR_PACIENTE_NAO_CADASTRADO};
+
+        // Verifica se o paciente já possui uma consulta marcada
+        if(await this.hasAgendamentosFuturos(cpf))
+            return {success: false, error: ErrorCodes.ERR_PACIENTE_AGENDADO};
 
         return this.consulta_builder.setCpf(cpf)
     }
@@ -83,15 +87,16 @@ export class ConsultaController{
     }
 
     /**
+     * @async
      * Verifica se um consulta sobrepoẽ alguma outra já agendada.
      * @param {Consulta} consulta - Instância de consulta para ser comparada
      * @returns {boolean} Retorna true se sobrepõe alguma consulta, senão retorna false
      */
-    isSobreposta(consulta){
-        for( const [chave, consultas_paciente] of this.consultas.entries())
-            for( const consulta_cadastrada of consultas_paciente)
-                if(consulta.isSobreposta(consulta_cadastrada))
-                    return true;
+    async isSobreposta(consulta){
+        const consultas = await Consulta.findAll();
+        for( const consulta_cadastrada of consultas)
+            if(consulta.isSobreposta(consulta_cadastrada))
+                return true;
 
         return false;
     }
@@ -99,85 +104,91 @@ export class ConsultaController{
     /**
     * Finaliza a criação da consulta e a adiciona ao mapa de consultas.
     * 
+    * @async
     * @returns {{success: boolean, error?: number}} Objeto contendo o status da operação e, em caso de erro, um código de erro.
     */
-    addConsulta(){
-        const resposta = this.consulta_builder.build();
+    async addConsulta(){
+        const resposta = await this.consulta_builder.build();
         if(!resposta.success)
             return resposta;
 
         const consulta = resposta.consulta;
 
-        if(this.hasAgendamentosFuturos(consulta.cpf_paciente))
+        if(await this.hasAgendamentosFuturos(consulta.cpf_paciente))
             return {success: false, error: ErrorCodes.ERR_CONSULTA_DUPLA};
 
-        if(this.isSobreposta(consulta))
+        if(await this.isSobreposta(consulta))
             return {success: false, error: ErrorCodes.ERR_CONSULTA_SOBREPOSTA};
 
-        if(!this.consultas.has(consulta.cpf_paciente))
-            this.consultas.set(consulta.cpf_paciente, []);
-
-        this.consultas.get(consulta.cpf_paciente).push(consulta);
-
+        try {
+            await consulta.save();
+        } catch (error) {
+            return {success: false, error: ErrorCodes.ERR_BD_FALHA_CONEXAO};
+        }
         return {success: true};
     }
 
     /**
     * Remove uma consulta específica de um paciente.
     * 
+    * @async
     * @param {string} cpf - CPF do paciente.
     * @param {string} data_consulta - Data da consulta no formato "dd/MM/yyyy".
     * @param {string} hora_inicial - Hora inicial no formato "HHmm".
     * @returns {{success: boolean, error?: number}} Objeto contendo o status da operação e, em caso de erro, um código de erro.
     */
-    removeConsulta(cpf, data_consulta, hora_inicial){
-
-        if(!this.consultas.has(cpf))
+    async removeConsulta(cpf, data_consulta, hora_inicial){
+        // Verifica se existe o paciente
+        if(!(await PacienteController.exists(cpf)))
             return {success: false, error: ErrorCodes.ERR_PACIENTE_NAO_CADASTRADO};
 
         const agora = DateTime.now();
         const data_hora_consulta = DateTime.fromFormat(data_consulta+hora_inicial, "dd/MM/yyyyHHmm");
 
+        // Verifica se a data da consulta a ser pesquisada é passada
         if(data_hora_consulta.diff(agora).toMillis() <= 0)
             return {success: false, error: ErrorCodes.ERR_CONSULTA_NAO_ENCONTRADA};
 
-        const consultas_paciente = this.consultas.get(cpf);
+        try{
+            const consultas_deletadas =  await Consulta.destroy({
+                where: {
+                    cpf_paciente: cpf,
+                    data_consulta: data_hora_consulta.toFormat("yyyy-MM-dd"),
+                    hora_inicial: data_hora_consulta.toFormat("HH:mm:00"),
+                }
+            });
 
-        for (let i = 0; i < consultas_paciente.length; i++) {
-            if(data_consulta === consultas_paciente[i].data_consulta.toFormat("dd/MM/yyyy") && consultas_paciente[i].hora_inicial.toFormat("HHmm") === hora_inicial){
-                consultas_paciente.splice(i, 1);
+            if(consultas_deletadas > 0)
                 return {success: true};
-            }
-        }
 
-        return {success: false, error: ErrorCodes.ERR_CONSULTA_NAO_ENCONTRADA};
+            return {success: false, error: ErrorCodes.ERR_CONSULTA_NAO_ENCONTRADA};
+        } catch(error) {
+            return {success: false, error: ErrorCodes.ERR_BD_FALHA_CONEXAO};
+        }
     }
 
     /**
-    * Remove todas as consultas de um paciente.
-    * 
-    * @param {string} cpf - CPF do paciente.
-    * @returns {{success: boolean, error?: number}} Objeto contendo o status da operação e, em caso de erro, um código de erro.
-    */
-    removeConsultasPaciente(cpf){
-
-        if(!this.consultas.has(cpf))
-            return {success: false, error: ErrorCodes.ERR_PACIENTE_NAO_CADASTRADO};
-
-        this.consultas.delete(cpf);
-        return {success: true};
+     * Obtem todas as consultas de um paciente
+     * 
+     * @async
+     * @param {String} cpf 
+     * @returns {Consulta[] | null} - Retorna as consultas de um paciente, se não tiver consultas retorna nulo
+     */
+    async #getConsultasPaciente(cpf){
+        return await Consulta.findAll({where: {cpf_paciente: cpf}});
     }
+
 
     /**
     * Verifica se o paciente possui agendamentos futuros.
     * 
+    * @async
     * @param {string} cpf - CPF do paciente.
     * @returns {boolean} Retorna true se houver agendamentos futuros, caso contrário, false.
     */
-    hasAgendamentosFuturos(cpf){
-
+    async hasAgendamentosFuturos(cpf){
         // Verifica se o paciente possui agendamentos futuros
-        const consultas_paciente = this.consultas.get(cpf);
+        const consultas_paciente = await this.#getConsultasPaciente(cpf);
         if (consultas_paciente && consultas_paciente.some((c) => !c.isConsultaPassada())) 
             return true;
 
@@ -187,48 +198,36 @@ export class ConsultaController{
     /**
     * Lista consultas futuras de um paciente.
     * 
+    * @async
     * @param {string} cpf - CPF do paciente.
     * @returns {{success: boolean, error?: number, consultas?:Array<Consulta>}} Objeto contendo o status da operação, as consultas futuras ou um código de erro.
     */
-    getAgendamentosFuturos(cpf){
-        if(!this.consultas.has(cpf))
+    async getAgendamentosFuturos(cpf){
+        const consultas = await this.#getConsultasPaciente(cpf);
+        if(!consultas)
             return {success: false, error: ErrorCodes.ERR_PACIENTE_NAO_CADASTRADO};
 
-        // Verifica se o paciente possui agendamentos futuros
-        const consultas_paciente = this.consultas.get(cpf);
-        var consultas_futuras = consultas_paciente.filter((consulta) => !consulta.isConsultaPassada());
-
+        const consultas_futuras = consultas.filter((consulta) => !consulta.isConsultaPassada());
         return {success: true, consultas: consultas_futuras};
     }
 
     /**
     * Lista todas as consultas, podendo filtrar por período.
     * 
-    * @param {PacienteController} paciente_controller - Controlador de pacientes para obter informações adicionais.
+    * @async
     * @param {boolean} filtro_periodo - Indica se deve filtrar por período.
     * @param {string} [data_inicial=null] - Data inicial no formato "dd/MM/yyyy".
     * @param {string} [data_final=null] - Data final no formato "dd/MM/yyyy".
     * @returns {string} String formatada com a lista de consultas.
     */
-    listarConsultas(paciente_controller, filtro_periodo=false, data_inicial=null, data_final=null){
+    async listarConsultas(filtro_periodo=false, data_inicial=null, data_final=null){
 
-        function concatDataEHora(consulta) {
-            return DateTime.fromObject({
-                year: consulta.data_consulta.year,
-                month: consulta.data_consulta.month,
-                day: consulta.data_consulta.day,
-                hour: consulta.hora_inicial.hour,
-                minute: consulta.hora_inicial.minute
-            }).toMillis();
-        }   
-
-        var lista_consultas = [...this.consultas.values()].flat();
+        let lista_consultas;
         if(filtro_periodo)
-            lista_consultas = this.#filtrarConsultas(lista_consultas, data_inicial, data_final);
+            lista_consultas = await this.#filtrarConsultas(data_inicial, data_final);
+        else
+            lista_consultas = await Consulta.findAll({order: [['data_consulta', 'ASC'], ['hora_inicial', 'ASC']]});
 
-        lista_consultas = lista_consultas.sort(
-            (a, b) => concatDataEHora(a) - concatDataEHora(b)
-        );
 
         // Cabeçalho
         var resultado = "-------------------------------------------------------------\n";
@@ -249,7 +248,7 @@ export class ConsultaController{
             resultado += `${consulta.hora_final.toFormat("HH:mm")} `;
             resultado += `${consulta.hora_final.diff(consulta.hora_inicial, ["hours", "minutes"]).toFormat("hh:mm")} `;
 
-            const paciente = paciente_controller.getPaciente(consulta.cpf_paciente);
+            const paciente = await PacienteController.getPaciente(consulta.cpf_paciente);
             resultado += `${paciente.nome.padEnd(21, ' ')} ${paciente.data_nasc.toFormat("dd/MM/yyyy")}\n`;
         }
         
@@ -261,6 +260,7 @@ export class ConsultaController{
     /**
     * Filtra uma lista de consultas por um intervalo de datas.
     * 
+    * @async
     * @param {Array<Consulta>} lista_consultas - Lista de consultas a ser filtrada.
     * @param {string} data_inicial - Data inicial do intervalo no formato "dd/MM/yyyy".
     * @param {string} data_final - Data final do intervalo no formato "dd/MM/yyyy".
@@ -269,20 +269,25 @@ export class ConsultaController{
     * 
     * @throws {Error} - Lança um erro se `data_inicial` ou `data_final` não forem fornecidas.
     */
-    #filtrarConsultas(lista_consultas, data_inicial, data_final){
+    async #filtrarConsultas(data_inicial, data_final){
         if(!(data_inicial && data_final))
             throw new Error("Listar consultas com período deve ter data inicial e final!");
+
 
         const data_inicial_formata  = DateTime.fromFormat(data_inicial, "dd/MM/yyyy");
         const data_final_formata    = DateTime.fromFormat(data_final, "dd/MM/yyyy");
 
-        return lista_consultas.filter(consulta => {
-            const data = consulta.data_consulta;
-            return (
-                data >= data_inicial_formata.startOf('day') &&
-                data <= data_final_formata.endOf('day')
-            );
+        const consultas = await Consulta.findAll({
+            where: {
+                data_consulta: {
+                    [Op.between]: [data_inicial_formata.toSQLDate(), data_final_formata.toSQLDate()],
+                }
+            },
+            order: [['data_consulta', 'ASC'], ['hora_inicial', 'ASC']], // Ordenação opcional
         });
+
+        return consultas;
+
     }
 
     /**
@@ -325,3 +330,5 @@ export class ConsultaController{
         return { success: true };
     }
 };
+
+export default new ConsultaController();
