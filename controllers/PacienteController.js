fe546025d5afea4b9ac3375ded39131f79294e3d
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-06 21:40:56 -0300

    :sparkles: feat: Controller adaptado para lidar com o banco de dados e chamdas assincronas

--- controllers/PacienteController.js
+++ controllers/PacienteController.js
@@ -1,32 +1,34 @@
 import {PacienteBuilder} from "../models/PacienteBuilder.js"
 import { DateTime } from "luxon";
 import { ErrorCodes } from "../utils/Error.js";
-import { ConsultaController } from "./ConsultaController.js";
+import ConsultaController from "./ConsultaController.js";
 import { Paciente } from "../models/Paciente.js";
 
+import { Sequelize } from "sequelize";
+
 /**
-* Controlador responsável por gerenciar as operações relacionadas aos pacientes.
+ * @class
+* Controlador Singleton responsável por gerenciar as operações relacionadas aos pacientes.
 * Inclui funcionalidades para adicionar, remover, validar e listar pacientes,
 * bem como verificar agendamentos associados.
 */
-export class PacienteController{
+class PacienteController{
 
     /**
     * Inicializa uma nova instância de PacienteController.
     * 
-    * - Armazena pacientes em um `Map` para busca eficiente.
     * - Utiliza um `PacienteBuilder` para gerenciar a construção de pacientes.
     */
     constructor() {
-        /** Armazena os pacientes em um hash map para pesquisas rápidas
-        * @type {Map<string, Paciente>}
-        */
-        this.pacientes = new Map();
-
+        if (PacienteController.instance) 
+            return PacienteController.instance; // Retorna a instância existente
+        
         /** 
         * @type {PacienteBuilder}
         */
         this.paciente_builder = new PacienteBuilder();
+
+        PacienteController.instance = this; // Salva a instância
     }
 
     /**
@@ -39,14 +41,15 @@ export class PacienteController{
     /**
     * Valida se o CPF é único e válido.
     * 
+    * @async
     * @param {string} cpf - O CPF do paciente a ser validado.
     * @returns {{success: boolean, error?: number}} - Um objeto contendo `success: true` se válido ou um erro com código correspondente.
     */
-    validaCpf(cpf){
+    async validaCpf(cpf){
         if(!this.paciente_builder.validaCpf(cpf))
             return {success: false, error: ErrorCodes.ERR_CPF_INVALIDO};
 
-        if(!this.pacientes.has(cpf))
+        if(!(await this.exists(cpf)))
             return {success: false, error: ErrorCodes.ERR_PACIENTE_NAO_CADASTRADO};
 
         return { success: true }
@@ -55,11 +58,12 @@ export class PacienteController{
     /**
     * Define o CPF no builder e verifica se já existe.
     * 
+    * @async
     * @param {string} cpf - O CPF do paciente.
     * @returns {{success: boolean, error?: number}} - Um objeto contendo `success: true` se válido ou um erro com código correspondente.
     */
-    setCpf(cpf){
-        if(this.pacientes.has(cpf))
+    async setCpf(cpf){
+        if(await this.exists(cpf))
             return {success: false, error: ErrorCodes.ERR_CPF_DUPLICADO}
 
         return this.paciente_builder.setCpf(cpf);
@@ -88,15 +92,16 @@ export class PacienteController{
     /**
     * Finaliza e adiciona o paciente ao registro.
     * 
+    * @async
     * @returns {{success: boolean, error?: number}} - Resultado da operação, indicando sucesso ou erro.
     */
-    addPaciente(){
-        const paciente = this.paciente_builder.build();
+    async addPaciente(){
+        const paciente = await this.paciente_builder.build();
         if(!paciente.success)
             return paciente;
 
-        this.pacientes.set(paciente.paciente.cpf, paciente.paciente);
-        this.paciente_builder.clear();
+        // Fazer tratamento de erros
+        await paciente.paciente.save();
 
         return {success: true};
     }
@@ -104,22 +109,24 @@ export class PacienteController{
     /**
     * Remove um paciente do registro, verificando agendamentos futuros antes.
     * 
+    * @async
     * @param {string} cpf - O CPF do paciente a ser removido.
-    * @param {ConsultaController} consulta_controller - Controlador de consultas para verificar agendamentos.
     * @returns {{success: boolean, error?: number}} - Resultado da operação, indicando sucesso ou erro.
     */
-    removePaciente(cpf, consulta_controller){
+    async removePaciente(cpf){
         
-        // VErifica se o paciente já existe
-        if (!this.pacientes.has(cpf)) 
+        const paciente = await Paciente.findOne({where: {cpf: cpf}});
+        // Verifica se o paciente existe
+        if (!paciente) 
             return { success: false, error: ErrorCodes.ERR_PACIENTE_NAO_CADASTRADO };
         
         // Verifica se o paciente possui agendamentos futuros
-        if(consulta_controller.hasAgendamentosFuturos(cpf))
+        if(await ConsultaController.hasAgendamentosFuturos(cpf))
             return { success: false, error: ErrorCodes.ERR_PACIENTE_AGENDADO };
         
-        this.pacientes.delete(cpf);
-        consulta_controller.removeConsultasPaciente(cpf);
+
+        // Remove o paciente e seus agendamentos
+        paciente.destroy();
 
         return { success: true};
     }
@@ -127,24 +134,24 @@ export class PacienteController{
     /**
     * Retorna um paciente pelo CPF.
     * 
+    * @async
     * @param {string} cpf - O CPF do paciente.
     * @returns {Paciente|null} - O paciente encontrado ou `null` se não existir.
     */
-    getPaciente(cpf){
-        if(!this.exists(cpf))
-            return null;
-
-        return this.pacientes.get(cpf);
+    async getPaciente(cpf){
+        return await Paciente.findOne({where: {cpf: cpf}});
     }
 
     /**
     * Verifica se um paciente existe no registro.
     * 
+    * @async
     * @param {string} cpf - O CPF do paciente.
     * @returns {boolean} - `true` se o paciente existe, caso contrário `false`.
     */
-    exists(cpf) {
-        return this.pacientes.has(cpf);
+    async exists(cpf) {
+        const paciente = await Paciente.findOne({where: {cpf: cpf}});
+        return paciente !== null;
     }
 
 
@@ -152,18 +159,16 @@ export class PacienteController{
     * Gera uma lista formatada de pacientes com suas informações e agendamentos futuros.
     * 
     * @param {Array<Paciente>} lista_pacientes - Lista de pacientes a serem formatados.
-    * @param {ConsultaController} consulta_controller - Controlador de consultas para verificar agendamentos futuros.
     * @returns {string} - A lista formatada.
     */
-    geraListaPacientes(lista_pacientes, consulta_controller){
+    async geraListaPacientes(lista_pacientes){
         // Cabeçalho da tabela
         let resultado = '------------------------------------------------------------\n';
         resultado +=    'CPF         Nome                           Dt.Nasc.    Idade\n';
         resultado +=    '------------------------------------------------------------\n';
 
         // Iterar sobre os pacientes
-        lista_pacientes.forEach((paciente) => {
-
+        for(const paciente of lista_pacientes){
             // calcular idade do paciente
             const hoje = DateTime.now();
             const idade = Math.floor(hoje.diff(paciente.data_nasc, 'years').years);
@@ -172,7 +177,7 @@ export class PacienteController{
             resultado += `${paciente.cpf} ${paciente.nome.padEnd(30, ' ')} ${paciente.data_nasc.toFormat('dd/MM/yyyy')} ${String(idade).padStart(6)}\n`;
 
             // Verificar se o paciente tem agendamento futuro
-            const agendamento = consulta_controller.getAgendamentosFuturos(paciente.cpf);
+            const agendamento = await ConsultaController.getAgendamentosFuturos(paciente.cpf);            
             if (agendamento.success) {
                 // Adicionar informações de agendamento futuro
                 agendamento.consultas.forEach((consulta) => {
@@ -180,7 +185,7 @@ export class PacienteController{
                     resultado += "".padEnd(12)+`${consulta.hora_inicial.toFormat('HH:mm')} às ${consulta.hora_final.toFormat('HH:mm')}\n`;
                 });
             }
-        });
+        };
 
         resultado +=  '------------------------------------------------------------\n';
 
@@ -190,25 +195,27 @@ export class PacienteController{
     /**
     * Retorna uma lista formatada de pacientes ordenados por CPF.
     * 
-    * @param {ConsultaController} consulta_controller - Controlador de consultas para verificar agendamentos futuros.
     * @returns {string} - A lista formatada.
     */
-    getPacientesOrdenadosPorCpf(consulta_controller){
+    async getPacientesOrdenadosPorCpf(){
         // Ordenar os pacientes pelo CPF
-        const pacientesOrdenados = [...this.pacientes.values()].sort((a, b) => a.cpf - b.cpf);
-        return this.geraListaPacientes(pacientesOrdenados, consulta_controller);
+        const pacientesOrdenados = await Paciente.findAll({
+            order: [[Sequelize.cast(Sequelize.col('cpf'), 'BIGINT'), 'ASC']]
+        });
+        return await this.geraListaPacientes(pacientesOrdenados);
     }
 
     /**
     * Retorna uma lista formatada de pacientes ordenados por Nome.
     * 
-    * @param {ConsultaController} consulta_controller - Controlador de consultas para verificar agendamentos futuros.
     * @returns {string} - A lista formatada.
     */
-    getPacientesOrdenadosPorNome(consulta_controller){
+    async getPacientesOrdenadosPorNome(){
         // Ordenar os pacientes pelo nome
-        const pacientesOrdenados = [...this.pacientes.values()].sort((a, b) => a.nome.localeCompare(b.nome));
-        return this.geraListaPacientes(pacientesOrdenados, consulta_controller);
+        const pacientesOrdenados = await Paciente.findAll({order: ['nome']});
+        return await this.geraListaPacientes(pacientesOrdenados);
     }
 
 }
+
+export default new PacienteController();
