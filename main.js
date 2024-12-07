91aecaf1501b109b9e7465c0ad1e6201cce79a09
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-06 22:19:02 -0300

    :recycle: refactor: Encapsulamento da função main e organização da autenticação

--- main.js
+++ main.js
@@ -3,37 +3,21 @@ import { CadastroPacientes } from './views/CadastroPacientes.js';
 import { Agendamento } from './views/Agendamento.js';
 
 import Database from './db/Database.js';
-import { ErrorCodes } from './utils/Error.js';
 
-const isAutenticado = await Database.autenticacao();
-if(!isAutenticado.sucess){
-    switch (isAutenticado.error) {
-        case ErrorCodes.ERR_BD_DESCONHECIDO:
-            console.log("Erro: Ocorreu um erro desconhecido ao se conectar com o banco de dados");
-            break;
+async function main(){
+
     
-        case ErrorCodes.ERR_BD_HOST_INVALIDO:
-            console.log("Erro: Host inválido, verifique o .env e as credenciais do seu banco de dados");
-            break;
-
-        case ErrorCodes.ERR_BD_LOGIN_INVALIDO:
-            console.log("Erro: Login inválido, verifique o .env e as credenciais do seu banco de dados");
-            break;
-
-        case ErrorCodes.ERR_BD_INEXISTENTE:
-            console.log("Erro: Nome do Banco de dados inválido, verifique o .env e as credenciais do seu banco de dados");
-            break;
-    }
-} else {
-
-    await Database.conexao.sync({ force: false });
-
     const Telas = { 
         "Menu":               new Menu(),
         "CadastroPacientes":  new CadastroPacientes(),
         "Agendamento":        new Agendamento(),
         "Fim":                false
     };
+    
+    if(!(await Telas.Menu.autenticar()))
+        return;
+
+    await Database.conexao.sync({ force: false });
 
     var tela_atual = Telas.Menu;
 
@@ -41,6 +25,8 @@ if(!isAutenticado.sucess){
         let proxima_tela= await tela_atual.main()
         tela_atual = Telas[proxima_tela];
     }
+
+    await Database.close();
 }
 
-await Database.close();
+await main();
