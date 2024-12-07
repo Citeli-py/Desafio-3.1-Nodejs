9b37ac1ffe5c4b3e750265552ead8454c86a1ae4
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-06 21:58:20 -0300

    :sparkles: feat: Autenticação com o banco

--- main.js
+++ main.js
@@ -2,22 +2,45 @@ import { Menu } from './views/Menu.js';
 import { CadastroPacientes } from './views/CadastroPacientes.js';
 import { Agendamento } from './views/Agendamento.js';
 
-import { PacienteController } from './controllers/PacienteController.js';
-import { ConsultaController } from './controllers/ConsultaController.js';
+import Database from './db/Database.js';
+import { ErrorCodes } from './utils/Error.js';
 
+const isAutenticado = await Database.autenticacao();
+if(!isAutenticado.sucess){
+    switch (isAutenticado.error) {
+        case ErrorCodes.ERR_BD_DESCONHECIDO:
+            console.log("Erro: Ocorreu um erro desconhecido ao se conectar com o banco de dados");
+            break;
+    
+        case ErrorCodes.ERR_BD_HOST_INVALIDO:
+            console.log("Erro: Host inválido, verifique o .env e as credenciais do seu banco de dados");
+            break;
 
-const consulta_controller = new ConsultaController();
-const pacientes_controller = new PacienteController();
+        case ErrorCodes.ERR_BD_LOGIN_INVALIDO:
+            console.log("Erro: Login inválido, verifique o .env e as credenciais do seu banco de dados");
+            break;
 
-const Telas = { "Menu":               new Menu(),
-                "CadastroPacientes":  new CadastroPacientes(pacientes_controller, consulta_controller),
-                "Agendamento":        new Agendamento(pacientes_controller, consulta_controller),
-                "Fim":                false
-            };
+        case ErrorCodes.ERR_BD_INEXISTENTE:
+            console.log("Erro: Nome do Banco de dados inválido, verifique o .env e as credenciais do seu banco de dados");
+            break;
+    }
+} else {
 
-var tela_atual = Telas.Menu;
+    await Database.conexao.sync({ force: false });
 
-while(tela_atual){
-    let proxima_tela= tela_atual.main()
-    tela_atual = Telas[proxima_tela];
+    const Telas = { 
+        "Menu":               new Menu(),
+        "CadastroPacientes":  new CadastroPacientes(),
+        "Agendamento":        new Agendamento(),
+        "Fim":                false
+    };
+
+    var tela_atual = Telas.Menu;
+
+    while(tela_atual){
+        let proxima_tela= await tela_atual.main()
+        tela_atual = Telas[proxima_tela];
+    }
 }
+
+await Database.close();
